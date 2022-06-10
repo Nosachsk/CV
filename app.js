6a30ac66d75ce2c59ad9e4e6ca4dd3800baf3a8f
Janosch <janosch.hoefer@gmail.com>
2022-06-10 20:55:55 +0200

    first commit test

---
+++ app.js
@@ -0,0 +1,23 @@
+
+    const el = document.getElementById('drag');
+      
+    $('div[class^="card"]').draggable(
+    {containment: '#content', snap: '#content', scroll:false},{stack: "div"},
+    {start: function(){
+    el.style.display = 'none';
+    }}
+    );
+
+    $('div[class^="card"]').resizable({
+        minHeight: 118,
+        minWidth: 200,
+        maxHeight: 300,
+        maxWidth: 600,
+    });
+    $('.ui-resizable').removeClass( "ui-resizable" ); //without this resizing the first div was making the second one move.
+
+    $('div[class^="minicard"]').draggable(
+    {containment: '#restraint', axis:'x', scroll:false},
+    {stack: "div"});
+
+    $('div[class^="login"]').draggable();
